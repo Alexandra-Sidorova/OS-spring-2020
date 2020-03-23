a54f06caf2e9035ee59b6bfffcb819393015430e
Alexandra-Sidorova <alexa-sanechka@ya.ru>
2020-03-23 15:18:31 +0300

    create child process - program_1

---
+++ lab3/program_1/main.c
@@ -0,0 +1,49 @@
+#include <windows.h>
+#include <stdio.h>
+
+void main()
+{
+	STARTUPINFO si;
+	PROCESS_INFORMATION pi;
+	ZeroMemory(&si, sizeof(si));
+	si.cb = sizeof(si);
+	ZeroMemory(&pi, sizeof(pi));
+
+	// Запустим дочерний процесс.
+	if (!CreateProcess( NULL, // модуль.
+		(LPSTR)"notepad.exe", // Командная строка не используется.
+		NULL,                 // Дескриптор процесса не наследуемый.
+		NULL,                 // Дескриптор потока не наследуемый.
+		FALSE,                // Установим наследование дескриптора в FALSE.
+		0,                    // Флажков создания нет.
+		NULL,                 // Используйте блок конфигурации родителя.
+		NULL,                 // Используйте стартовый каталог родителя.
+		&si,                  // Указатель на структуру STARTUPINFO.
+		&pi)                 // Указатель на структуру PROCESS_INFORMATION.
+		)
+	{
+		printf("[ERROR] Process wasn't created!");
+		return;
+	}
+	
+
+	// Ждать до тех пор, пока дочерний процесс не выйдет из работы.
+	if (WaitForSingleObject(pi.hProcess, INFINITE) == WAIT_FAILED)
+	{
+		printf("[ERROR] Wait failed!");
+		return;
+	}
+
+	// Закроем дескрипторы процесса и потока.
+	if (!CloseHandle(pi.hProcess))
+	{
+		printf("[ERROR] Closing process failed!");
+		return;
+	}
+	
+	if( !CloseHandle(pi.hThread))
+	{
+		printf("[ERROR] Closing thread failed!");
+		return;
+	}
+}
